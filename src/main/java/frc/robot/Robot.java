176a59d77acbeac9c999524826319e2983bfd225
Aden Cross <adenrudy23@gmail.com>
2022-02-26 13:13:20 -0800

    Added LimelightSubsystem and line to start logitech camera.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@
 package frc.robot;
 
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -37,6 +38,9 @@ public class Robot extends A05Robot
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = new RobotContainer();
+
+        // Start logitech camera
+        CameraServer.startAutomaticCapture();
     }
     
     
