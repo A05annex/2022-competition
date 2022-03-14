93df16061e029e6ee9e10ae774db02dbb89e46df
Aden Cross <adenrudy23@gmail.com>
2022-03-14 16:27:07 -0700

    Added and bound LimelightDoubleShootCommand. New shoot button bindings: B shoots, A double shoots, and Y shoots at set speeds.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -105,14 +105,14 @@ public class RobotContainer
      */
     private void configureButtonBindings()
     {
-        m_xboxBack.whenPressed(new InstantCommand(m_navx::initializeHeadingAndNav)); // Reset the NavX field relativity
-        m_xboxX.whenPressed(new CollectorJerkCommand());
-//        m_xboxB.whenPressed(new ShooterCommand(ShooterSubsystem.AUTO_BALL_FRONT, ShooterSubsystem.AUTO_BALL_REAR));
+        // Shooting bindings
         m_xboxB.whenPressed(new LimelightShootCommand());
+        m_xboxA.whenPressed(new LimelightDoubleShootCommand());
         m_xboxY.whenPressed(new ShooterSetSpeedCommand());
-//        m_xboxA.whenPressed(new ShooterCommand(ShooterSubsystem.DUMP_SPEED_FRONT, ShooterSubsystem.DUMP_SPEED_REAR));
-        m_xboxA.whenPressed(new DoubleShootCommand(ShooterSubsystem.AUTO_BALL_FRONT, ShooterSubsystem.AUTO_BALL_REAR));
-        m_hangA.whenPressed(new AutoDoubleShootCommand());
+
+        // Other bindings for the drive controller
+        m_xboxX.whenPressed(new CollectorJerkCommand());
+        m_xboxBack.whenPressed(new InstantCommand(m_navx::initializeHeadingAndNav)); // Reset the NavX field relativity
     }
     
 
