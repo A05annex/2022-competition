b88b0bc0e4d49c1bb26e9cecb8d7039c58ee5f5f
Aden Cross <adenrudy23@gmail.com>
2022-04-04 18:40:02 -0700

    New 4ball auto, changed auto order

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -138,12 +138,13 @@ public final class Constants {
 
     // enum that contains autos
     public enum AutonomousPath {
-        FOUR_BALL_LIMELIGHT("4Ball Limelight", 0, "2022_4ball_limelight.json"),
+        FOUR_BALL("4Ball", 0, "2022_4ball.json"),
         TWO_BALL_A("2Ball A", 1, "2022_2ball_A.json"),
         TWO_BALL_B("2Ball B", 2, "2022_2ball_B.json"),
-        TWO_BALL_C("2Ball C", 3, "2022_2ball_C.json");
+        TWO_BALL_C("2Ball C", 3, "2022_2ball_C.json"),
+        THREE_BALL_WESTVALLEY("3Ball West Valley", 0, "2022_3ball_westvalley.json");
 
-        static AutonomousPath AUTONOMOUS_PATH = AutonomousPath.TWO_BALL_A;
+        static AutonomousPath AUTONOMOUS_PATH = AutonomousPath.TWO_BALL_B;
 
         private final String m_pathName;
         private final int m_id;
