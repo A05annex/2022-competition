94353c6e5173d10e947f09d8108187c0416e68ae
Aden Cross <adenrudy23@gmail.com>
2022-04-07 15:34:57 -0700

    Added 1ball auto

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -142,7 +142,8 @@ public final class Constants {
         TWO_BALL_A("2Ball A", 1, "2022_2ball_A.json"),
         TWO_BALL_B("2Ball B", 2, "2022_2ball_B.json"),
         TWO_BALL_C("2Ball C", 3, "2022_2ball_C.json"),
-        THREE_BALL_WESTVALLEY("3Ball West Valley", 4, "2022_3ball_westvalley.json");
+        ONE_BALL("1Ball Mid", 4, "2022_1ball.json"),
+        THREE_BALL_WESTVALLEY("3Ball West Valley", 5, "2022_3ball_westvalley.json");
 
         static AutonomousPath AUTONOMOUS_PATH = AutonomousPath.TWO_BALL_B;
 
